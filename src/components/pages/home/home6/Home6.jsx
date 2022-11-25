aa79b918d8a8f88afe819204ff23c190d5297545
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2022-11-25 18:59:55 +0530

    1 file modified, 2 added

--- src/components/pages/home/home6/Home6.jsx
+++ src/components/pages/home/home6/Home6.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Banner from '../../../common/banner/Banner';
 import Hero from '../hero/Hero';
 import RecentNew from './Home6RecentNew/RecentNew';
+import SimpleCart from './Home6SimpleCart/SimpleCart';
 
 export default function Home6() {
   return (
     <div>
       <Hero/>
+      <SimpleCart/>
       <RecentNew/>
       <Banner/>
 
---
+++ src/components/pages/home/home6/Home6SimpleCart/SimpleCart.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+export default function SimpleCart() {
+  return (
+    <div className="cartttt">
+      <h2>Simple cart</h2>
+    </div>
+  );
+}
